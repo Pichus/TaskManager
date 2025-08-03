f5b276ccf2b5737afd1b10f59ad7316cca4d9541
pichus <dimatyzuk@gmail.com>
2025-08-03 15:04:20 +0300

    remove email from RegisterRequest.cs

--- src/TaskManager.UseCases/Identity/Register/RegisterRequest.cs
+++ src/TaskManager.UseCases/Identity/Register/RegisterRequest.cs
@@ -5,8 +5,5 @@ namespace TaskManager.UseCases.Identity.Register;
 public class RegisterRequest
 {
     public string UserName { get; set; }
-
-    [EmailAddress] public string Email { get; set; }
-
     public string Password { get; set; }
 }
