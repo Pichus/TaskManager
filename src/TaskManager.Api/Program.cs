ea689921e0b1f00721469e9864f8dd2847b3f817
pichus <dimatyzuk@gmail.com>
2025-08-03 12:51:35 +0300

    add jwt auth setup for api

--- src/TaskManager.Api/Program.cs
+++ src/TaskManager.Api/Program.cs
@@ -1,11 +1,44 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 using TaskManager.Extensions;
 using TaskManager.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwtOptions =>
+    {
+        jwtOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ValidAudience = config["JwtSettings:Audience"],
+            ValidIssuer = config["JwtSettings:Issuer"]
+        };
+    });
 
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApiDocument();
+builder.Services.AddOpenApiDocument(document =>
+{
+    document.AddSecurity("Bearer", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+    {
+        Type = OpenApiSecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        BearerFormat = "JWT",
+        Description = "Type into the textbox: {your JWT token}."
+    });
+
+    document.OperationProcessors.Add(
+        new AspNetCoreOperationSecurityScopeProcessor("Bearer"));
+});
 
 builder.Services.AddInfrastructureServices(builder.Configuration, builder.Environment.EnvironmentName);
 
@@ -19,6 +52,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
