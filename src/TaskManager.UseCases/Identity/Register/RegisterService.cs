a737d78ca3897ec64f2be97542dda1f3734be36e
pichus <dimatyzuk@gmail.com>
2025-08-03 15:05:14 +0300

    remove email from RegisterService.cs

--- src/TaskManager.UseCases/Identity/Register/RegisterService.cs
+++ src/TaskManager.UseCases/Identity/Register/RegisterService.cs
@@ -17,7 +17,6 @@ public class RegisterService : IRegisterService
         var newUser = new TaskManagerUser
         {
             UserName = request.UserName,
-            Email = request.Email
         };
 
         var createUserResult = await _userManager.CreateAsync(newUser);
