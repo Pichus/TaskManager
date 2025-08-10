a19a723a58f951bbcb74ed93a1eac097fca53748
pichus <dimatyzuk@gmail.com>
2025-08-10 18:01:41 +0300

    add GetProjectMemberResponse

---
+++ src/TaskManager.Api/ProjectMembers/Get/GetProjectMemberResponse.cs
@@ -0,0 +1,9 @@
+namespace TaskManager.ProjectMembers.Get;
+
+public class GetProjectMemberResponse
+{
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string ProjectRole { get; set; }
+}
