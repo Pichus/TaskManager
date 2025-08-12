95f30b31254806c91dcb5dcdbb6828c3d2ad246b
pichus <dimatyzuk@gmail.com>
2025-08-12 21:52:54 +0300

    add logging to project member service

--- src/TaskManager.UseCases/ProjectMembers/ProjectMemberService.cs
+++ src/TaskManager.UseCases/ProjectMembers/ProjectMemberService.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 using TaskManager.Core.ProjectAggregate;
 using TaskManager.Infrastructure.Data;
 using TaskManager.Infrastructure.Identity.CurrentUser;
@@ -14,110 +15,160 @@ public class ProjectMemberService : IProjectMemberService
 {
     private readonly ICurrentUserService _currentUserService;
     private readonly AppDbContext _dbContext;
+    private readonly ILogger _logger;
     private readonly IProjectMemberRepository _projectMemberRepository;
     private readonly IProjectRepository _projectRepository;
     private readonly UserManager<TaskManagerUser> _userManager;
 
     public ProjectMemberService(ICurrentUserService currentUserService, IProjectRepository projectRepository,
         IProjectMemberRepository projectMemberRepository, UserManager<TaskManagerUser> userManager,
-        AppDbContext dbContext)
+        AppDbContext dbContext, ILogger logger)
     {
         _currentUserService = currentUserService;
         _projectRepository = projectRepository;
         _projectMemberRepository = projectMemberRepository;
         _userManager = userManager;
         _dbContext = dbContext;
+        _logger = logger;
     }
 
     public async Task<Result<IEnumerable<ProjectMemberWithUser>>> GetProjectMembersAsync(long projectId)
     {
+        _logger.LogInformation("Getting Project: {ProjectId} members", projectId);
+        
         var currentUserId = _currentUserService.UserId;
 
         if (currentUserId is null)
+        {
+            _logger.LogInformation("Getting project members failed - user unauthenticated");
             return Result<IEnumerable<ProjectMemberWithUser>>.Failure(UseCaseErrors.Unauthenticated);
+        }
 
         var project = await _projectRepository.FindByIdAsync(projectId);
 
         if (project is null)
+        {
+            _logger.LogInformation("Getting project members failed - project not found");            
             return Result<IEnumerable<ProjectMemberWithUser>>.Failure(GetProjectMembersErrors.ProjectNotFound);
+        }
 
         var canGetProjectMembers = project.LeadUserId == currentUserId ||
                                    await _projectMemberRepository.IsUserProjectMember(currentUserId, projectId);
 
         if (!canGetProjectMembers)
+        {
+            _logger.LogInformation("Getting project members failed - access denied");   
             return Result<IEnumerable<ProjectMemberWithUser>>.Failure(GetProjectMembersErrors.AccessDenied);
+        }
 
         var projectMembers =
             await _projectMemberRepository.GetProjectMembersWithUsersAsync(projectId);
 
+        _logger.LogInformation("Got project members successfully");
         return Result<IEnumerable<ProjectMemberWithUser>>.Success(projectMembers);
     }
 
     public async Task<Result> UpdateProjectMemberAsync(long projectId, string memberId, ProjectRole projectRole)
     {
+        _logger.LogInformation("Updating Project: {ProjectId} Member: {MemberId}", projectId, memberId);
+        
         var currentUserId = _currentUserService.UserId;
 
         if (currentUserId is null)
+        {
+            _logger.LogInformation("Updating project member failed - user unauthenticated");
             return Result.Failure(UseCaseErrors.Unauthenticated);
+        }
 
         var memberUser = await _userManager.FindByIdAsync(memberId);
 
         if (memberUser is null)
+        {
+            _logger.LogInformation("Updating project member failed - member not found");
             return Result.Failure(UpdateProjectMemberErrors.MemberNotFound);
+        }
 
         var project = await _projectRepository.FindByIdAsync(projectId);
 
         if (project is null)
+        {
+            _logger.LogInformation("Updating project member failed - project not found");
             return Result.Failure(UpdateProjectMemberErrors.ProjectNotFound);
+        }
 
         var isCurrentUserProjectLead = currentUserId == project.LeadUserId;
 
         if (!isCurrentUserProjectLead)
+        {
+            _logger.LogInformation("Updating project member failed - access denied");
             return Result.Failure(UpdateProjectMemberErrors.AccessDenied);
+        }
 
         var projectMember = await _projectMemberRepository.GetByProjectIdAndMemberIdAsync(projectId, memberId);
 
         var isUserProjectMember = projectMember is not null;
 
         if (!isUserProjectMember)
+        {
+            _logger.LogInformation("Updating project member failed - user is not a member");
             return Result.Failure(UpdateProjectMemberErrors.UserIsNotAProjectMember);
+        }
 
         if (projectMember.ProjectRole == projectRole)
+        {
+            _logger.LogInformation("Updating project member failed - member already has this role");
             return Result.Failure(UpdateProjectMemberErrors.MemberAlreadyHasThisRole);
+        }
 
         projectMember.ProjectRole = projectRole;
         await _dbContext.SaveChangesAsync();
 
+        _logger.LogInformation("Updated project member successfully");
         return Result.Success();
     }
 
     public async Task<Result> DeleteAsync(long projectId, string memberId)
     {
+        _logger.LogInformation("Deleting Project: {ProjectId} Member: {MemberId}", projectId, memberId);
+        
         var currentUserId = _currentUserService.UserId;
 
         if (currentUserId is null)
+        {
+            _logger.LogInformation("Deleting project member failed - user unauthenticated");
             return Result.Failure(UseCaseErrors.Unauthenticated);
+        }
 
         var project = await _projectRepository.FindByIdAsync(projectId);
 
         if (project is null)
+        {
+            _logger.LogInformation("Deleting project member failed - project not found");
             return Result.Failure(DeleteProjectMemberErrors.ProjectNotFound);
+        }
 
         var isCurrentUserProjectLead = project.LeadUserId == currentUserId;
 
         if (!isCurrentUserProjectLead)
+        {
+            _logger.LogInformation("Deleting project member failed - access denied");   
             return Result.Failure(DeleteProjectMemberErrors.AccessDenied);
+        }
 
         var projectMember = await _projectMemberRepository.GetByProjectIdAndMemberIdAsync(projectId, memberId);
 
         var isUserProjectMember = projectMember is not null;
 
         if (!isUserProjectMember)
+        {
+            _logger.LogInformation("Deleting project member failed - member not found");
             return Result.Failure(DeleteProjectMemberErrors.ProjectMemberNotFound);
+        }
 
         _projectMemberRepository.Delete(projectMember);
         await _dbContext.SaveChangesAsync();
 
+        _logger.LogInformation("Deleted project member successfully");
         return Result.Success();
     }
 }
